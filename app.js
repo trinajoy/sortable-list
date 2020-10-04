1f24c1d95ffdcc384c42236090ccda86a4d37b35
Katrina H <katrina.hitchcock@gmail.com>
2020-10-04 17:51:22 +0200

    update styles

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-var draggable_list = document.getElementById("draggable-list");
+const draggable_list = document.getElementById("draggable-list");
 const check = document.getElementById("check");
 
-var richestPeople = ["Bill Gates", "Mark Zuckerberg", "Jeff Bezos", "Larry L"];
+const richestPeople = [
+  "Jeff Bezos",
+  "Bill Gates",
+  "Warren Buffett",
+  "Bernard Arnault",
+  "Carlos Slim Helu",
+  "Amancio Ortega",
+  "Larry Ellison",
+  "Mark Zuckerberg",
+  "Michael Bloomberg",
+  "Larry Page",
+];
 
 const listItems = [];
 let dragStartIndex;
